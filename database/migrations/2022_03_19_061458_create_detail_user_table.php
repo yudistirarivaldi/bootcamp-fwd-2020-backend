038940fb6918a868be3de728b9f47170acec505c
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2022-03-19 16:16:40 +0800

    Migration and Foreign key

---
+++ database/migrations/2022_03_19_061458_create_detail_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            // $table->integer('user_id');
+            // $table->integer('type_user_id');
+            $table->foreignId('user_id')->nullable()->index('fk_detail_user_to_users');
+            $table->foreignId('type_user_id')->nullable()->index('fk_detail_user_to_type_user');
+            $table->string('contact')->nullable();
+            $table->longText('address')->nullable();
+            $table->longText('photo')->nullable();
+            $table->enum('gender', [1,2])->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_user');
+    }
+}
