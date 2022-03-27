8c8176f25ade28e35123cc71e9614dfb3255af3f
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2022-03-27 16:10:35 +0800

    Make request and folder final

---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use App\Models\User;
+
+use Symfony\Component\HttpFoundation\Response;
+
+// This only on update request
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required', 'string', 'max:255'
+            ],
+            'email' => [
+                'required', 'email', 'max:255', Rule::unique('users')->ignore($this->user)
+                // Rule unique only works for other second id
+            ],
+             'password' => [
+                'min:8', 'string','max:255', 'mixedCase'
+            ],
+        ];
+    }
+}
